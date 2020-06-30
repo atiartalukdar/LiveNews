8cbbf634abc5decc57639672090506d503efef2b
Atiar Talukdar <swajan.talukdar@gmail.com>
2020-07-01 04:44:00 +0600

    Data Model updated

--- app/src/main/java/co/mubashirgulf/app/models/TradingModel.java
+++ app/src/main/java/co/mubashirgulf/app/models/TradingModel.java
@@ -1,9 +1,11 @@
 package co.mubashirgulf.app.models;
+
 import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class TradingModel {
+
     @SerializedName("status")
     @Expose
     private String status;
@@ -12,10 +14,10 @@ public class TradingModel {
     private List<AllRecord> allRecord = null;
     @SerializedName("most_read")
     @Expose
-    private List<MostRead> mostRead = null;
+    private List<AllRecord> mostRead = null;
     @SerializedName("latest_articles")
     @Expose
-    private List<LatestArticle> latestArticles = null;
+    private List<AllRecord> latestArticles = null;
 
     public String getStatus() {
         return status;
@@ -33,165 +35,20 @@ public class TradingModel {
         this.allRecord = allRecord;
     }
 
-    public List<MostRead> getMostRead() {
+    public List<AllRecord> getMostRead() {
         return mostRead;
     }
 
-    public void setMostRead(List<MostRead> mostRead) {
+    public void setMostRead(List<AllRecord> mostRead) {
         this.mostRead = mostRead;
     }
 
-    public List<LatestArticle> getLatestArticles() {
+    public List<AllRecord> getLatestArticles() {
         return latestArticles;
     }
 
-    public void setLatestArticles(List<LatestArticle> latestArticles) {
+    public void setLatestArticles(List<AllRecord> latestArticles) {
         this.latestArticles = latestArticles;
-    }
-
-
-
-    public class MostRead {
-
-        @SerializedName("post_id")
-        @Expose
-        private String postId;
-        @SerializedName("post_title")
-        @Expose
-        private String postTitle;
-        @SerializedName("post_content")
-        @Expose
-        private String postContent;
-        @SerializedName("post_date")
-        @Expose
-        private String postDate;
-        @SerializedName("image")
-        @Expose
-        private String image;
-
-        @SerializedName("post_link")
-        @Expose
-        private String postLink;
-
-
-        public String getPostId() {
-            return postId;
-        }
-
-        public void setPostId(String postId) {
-            this.postId = postId;
-        }
-
-        public String getPostTitle() {
-            return postTitle;
-        }
-
-        public void setPostTitle(String postTitle) {
-            this.postTitle = postTitle;
-        }
-
-        public String getPostContent() {
-            return postContent;
-        }
-
-        public void setPostContent(String postContent) {
-            this.postContent = postContent;
-        }
-
-        public String getPostDate() {
-            return postDate;
-        }
-
-        public void setPostDate(String postDate) {
-            this.postDate = postDate;
-        }
-
-        public String getImage() {
-            return image;
-        }
-
-        public void setImage(String image) {
-            this.image = image;
-        }
-
-        public String getPostLink() {
-            return postLink;
-        }
-
-        public void setPostLink(String postLink) {
-            this.postLink = postLink;
-        }
-    }
-
-    public class LatestArticle {
-
-        @SerializedName("post_id")
-        @Expose
-        private String postId;
-        @SerializedName("post_title")
-        @Expose
-        private String postTitle;
-        @SerializedName("post_content")
-        @Expose
-        private String postContent;
-        @SerializedName("post_date")
-        @Expose
-        private String postDate;
-        @SerializedName("image")
-        @Expose
-        private String image;
-
-        @SerializedName("post_link")
-        @Expose
-        private String postLink;
-
-        public String getPostId() {
-            return postId;
-        }
-
-        public void setPostId(String postId) {
-            this.postId = postId;
-        }
-
-        public String getPostTitle() {
-            return postTitle;
-        }
-
-        public void setPostTitle(String postTitle) {
-            this.postTitle = postTitle;
-        }
-
-        public String getPostContent() {
-            return postContent;
-        }
-
-        public void setPostContent(String postContent) {
-            this.postContent = postContent;
-        }
-
-        public String getPostDate() {
-            return postDate;
-        }
-
-        public void setPostDate(String postDate) {
-            this.postDate = postDate;
-        }
-
-        public String getImage() {
-            return image;
-        }
-
-        public void setImage(String image) {
-            this.image = image;
-        }
-
-        public String getPostLink() {
-            return postLink;
-        }
-
-        public void setPostLink(String postLink) {
-            this.postLink = postLink;
-        }
     }
 
     public class AllRecord {
@@ -208,12 +65,15 @@ public class TradingModel {
         @SerializedName("post_date")
         @Expose
         private String postDate;
-        @SerializedName("image")
-        @Expose
-        private String image;
         @SerializedName("post_link")
         @Expose
         private String postLink;
+        @SerializedName("image")
+        @Expose
+        private String image;
+        @SerializedName("comment")
+        @Expose
+        private List<Comment> comment = null;
 
         public String getPostId() {
             return postId;
@@ -247,6 +107,14 @@ public class TradingModel {
             this.postDate = postDate;
         }
 
+        public String getPostLink() {
+            return postLink;
+        }
+
+        public void setPostLink(String postLink) {
+            this.postLink = postLink;
+        }
+
         public String getImage() {
             return image;
         }
@@ -255,12 +123,85 @@ public class TradingModel {
             this.image = image;
         }
 
-        public String getPostLink() {
-            return postLink;
+        public List<Comment> getComment() {
+            return comment;
         }
 
-        public void setPostLink(String postLink) {
-            this.postLink = postLink;
+        public void setComment(List<Comment> comment) {
+            this.comment = comment;
         }
+
     }
+
+    public class Comment {
+
+        @SerializedName("comment_id")
+        @Expose
+        private String commentId;
+        @SerializedName("comment_author")
+        @Expose
+        private String commentAuthor;
+        @SerializedName("comment_author_email")
+        @Expose
+        private String commentAuthorEmail;
+        @SerializedName("comment_author_url")
+        @Expose
+        private String commentAuthorUrl;
+        @SerializedName("comment_content")
+        @Expose
+        private String commentContent;
+        @SerializedName("comment_date")
+        @Expose
+        private String commentDate;
+
+        public String getCommentId() {
+            return commentId;
+        }
+
+        public void setCommentId(String commentId) {
+            this.commentId = commentId;
+        }
+
+        public String getCommentAuthor() {
+            return commentAuthor;
+        }
+
+        public void setCommentAuthor(String commentAuthor) {
+            this.commentAuthor = commentAuthor;
+        }
+
+        public String getCommentAuthorEmail() {
+            return commentAuthorEmail;
+        }
+
+        public void setCommentAuthorEmail(String commentAuthorEmail) {
+            this.commentAuthorEmail = commentAuthorEmail;
+        }
+
+        public String getCommentAuthorUrl() {
+            return commentAuthorUrl;
+        }
+
+        public void setCommentAuthorUrl(String commentAuthorUrl) {
+            this.commentAuthorUrl = commentAuthorUrl;
+        }
+
+        public String getCommentContent() {
+            return commentContent;
+        }
+
+        public void setCommentContent(String commentContent) {
+            this.commentContent = commentContent;
+        }
+
+        public String getCommentDate() {
+            return commentDate;
+        }
+
+        public void setCommentDate(String commentDate) {
+            this.commentDate = commentDate;
+        }
+
+    }
+
 }
